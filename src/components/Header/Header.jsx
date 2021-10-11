ca55fab4a53444df3db9a81e0edb6f819fa4e85b
Vladimir <parnuk1996@gmail.com>
2021-10-11 19:36:36 +0300

    Add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return <header className='header'>
+        <img src="https://cdn.logo.com/hotlink-ok/logo-social.png"/>
+        <div className='link'>
+            <ul>
+                <li>s</li>
+                <li>d</li>
+                <li>f</li>
+                <li>g</li>
+                <li>h</li>
+            </ul>
+        </div>
+    </header>;
+}
+export default Header
